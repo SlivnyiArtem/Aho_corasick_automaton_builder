b62c1a3d00935ce9f152580265d072361b2f1fb7
SlivnyiArtem <slivnay1965@gmail.com>
2022-11-13 21:24:34 +0500

    working text table

--- algorithm/algo_window.py
+++ algorithm/algo_window.py
@@ -1,4 +1,3 @@
-import networkx as nx
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
@@ -26,7 +25,7 @@ def calculate(text):
         else:
             visualize_dict[(node.value, "")] = "lambda"
     graph = graph_constructor.form_graph(visualize_dict)
-    return graph, nwx.planar_layout(graph), visualize_dict
+    return graph, nwx.planar_layout(graph), visualize_dict, prefixes, node_dict, abc
 
 
 
@@ -52,25 +51,100 @@ class AhoKorasicWindow(QFrame):
 
         self.graph = plt.figure()
         self.cnv = cnv(self.graph)
+
+
+        self.table = QTableWidget(self)
+
+
+
+
+
+
+        '''
+        data = {'Kitty': ['1', '2', '3', '3'],
+                'Cat': ['4', '5', '6', '2'],
+                'Meow': ['7', '8', '9', '5'],
+                'Purr': ['4', '3', '4', '8'], }
+        # Create Empty 5x5 Table
+        self.table = QtGui.QTextTable(PrettyWidget)
+        self.table.setRowCount(5)
+        self.table.setColumnCount(5)
+        # Enter data onto Table
+        horHeaders = []
+        for n, key in enumerate(sorted(data.keys())):
+            horHeaders.append(key)
+            for m, item in enumerate(data[key]):
+                newitem = QtGui.QTableWidgetItem(item)
+                self.table.setItem(m, n, newitem)
+        # Add Header
+        self.table.setHorizontalHeaderLabels(horHeaders)
+        # Adjust size of Table
+        self.table.resizeColumnsToContents()
+        self.table.resizeRowsToContents()
+        '''
+
+
         grid = QGridLayout()
         self.setLayout(grid)
         grid.addWidget(self.cnv)
-        grid.addWidget(self.restart_btn, 0,0,5,5)
+        grid.addWidget(self.restart_btn, 100,100,5,5)
+        grid.addWidget(self.table, 100, 0)
+
+
+
+
 
         self.show()
-
         self.re_translate_ui()
         self.get_text()
 
     def get_text(self):
         text, ok_pressed = QInputDialog.getText(self, "Ввести словарь", "Словарь:", QLineEdit.Normal, "")
         if ok_pressed and text != '':
-            graph, graph_pos, labels = calculate(text)
+            graph, graph_pos, labels, prefixes, node_dict, abc = calculate(text)
 
 
             nwx.draw_planar(graph, with_labels=True)
             nwx.draw_networkx_edge_labels(graph, graph_pos, edge_labels=labels)
             self.cnv.draw_idle()
+            abc.sort()
+            print(prefixes)
+            print(node_dict)
+            print(abc)
+
+            # cnt1 = 1 + len(abc) + 1
+            cnt2 = len(prefixes)
+            list1 = ["NodeValue"] + abc + ["lambda"]
+            cnt1 = len(list1)
+
+
+            self.table.setColumnCount(cnt1)
+            self.table.setRowCount(cnt2)
+            self.table.setHorizontalHeaderLabels(list1)
+            print("------")
+            for i in range(cnt2):
+                print(node_dict.get(prefixes[i]).prefix_links)
+                for j in range(cnt1):
+                    #j - текущее коммандное слово
+                    #i - текущий префикс
+
+                    if j == 0:
+                        value = prefixes[i]
+                    elif j == cnt1 - 1:
+                        value = node_dict.get(prefixes[i]).suffix_link
+                    else:
+                        value = node_dict.get(prefixes[i]).prefix_links.get(list1[j])
+                    if value is None:
+                        value = "lambda"
+
+
+                    # print("VVV")
+                    # print(node_dict.get(prefixes[i]).value)
+                    # print(node_dict.get(prefixes[i]).prefix_links)
+                    # print(node_dict.get(prefixes[i]).suffix_link)
+                    self.table.setItem(i, j, QTableWidgetItem(value))
+            self.table.resizeColumnsToContents()
+            self.table.resizeColumnsToContents()
 
 
     def re_translate_ui(self):
